a7fdabf6a8b1280f67edd32adb02e26e35cc254e
jualfel30-eng <jualfel3.0@gmail.com>
2025-09-05 17:46:13 -0400

    Delete food-inventory-saas directory

--- food-inventory-saas/src/decorators/permissions.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const PERMISSIONS_KEY = 'permissions';
-
-export const RequirePermissions = (module: string, actions: string[]) =>
-  SetMetadata(PERMISSIONS_KEY, { module, actions });
-
