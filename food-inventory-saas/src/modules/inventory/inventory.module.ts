ee60d0535cfc57a6987313f042dab88ba5e4dd78
jualfel30-eng <jualfel3.0@gmail.com>
2025-09-05 17:54:29 -0400

    Add files via upload

---
+++ food-inventory-saas/src/modules/inventory/inventory.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { InventoryController } from './inventory.controller';
+import { InventoryService } from './inventory.service';
+import { Inventory, InventorySchema } from '../../schemas/inventory.schema';
+import { InventoryMovement, InventoryMovementSchema } from '../../schemas/inventory.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Inventory.name, schema: InventorySchema },
+      { name: InventoryMovement.name, schema: InventoryMovementSchema },
+    ]),
+  ],
+  controllers: [InventoryController],
+  providers: [InventoryService],
+  exports: [InventoryService],
+})
+export class InventoryModule {}
+
